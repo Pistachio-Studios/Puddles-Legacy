0b8e5f3db706dd74d4a98c5d975447880f02f7cf
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2024-04-08 17:54:37 +0200

    load csv

--- src/Core/DialogManager.cpp
+++ src/Core/DialogManager.cpp
@@ -22,14 +22,22 @@ bool DialogManager::Awake(pugi::xml_node& config) {
     bool ret = true;
 
     // Load dialog data from CSV file using rapidcsv
-    LoadDialogs("bin/dialogs.csv", dialogs);
+    LoadDialogs("dialogs.csv", dialogs);
 
-    // Check if the dialogs were loaded successfully
-    if (dialogs.empty()) {
-        // Handle the case when the file failed to load
-        LOG("Failed to load dialog data from dialogs.csv file");
-        ret = false;
+    /*
+    // TODO remove this debug code
+    for (const auto& pair : dialogs) {
+        const Dialog& dialog = pair.second;
+        std::cout << "Dialog ID: " << dialog.id << std::endl;
+        std::cout << "Character: " << dialog.character << std::endl;
+        std::cout << "ES: " << dialog.ES << std::endl;
+        std::cout << "EN: " << dialog.EN << std::endl;
+        std::cout << "CAT: " << dialog.CAT << std::endl;
+        std::cout << "Type: " << (dialog.type == DialogType::DIALOG ? "DIALOG" : "CHOICE") << std::endl;
+        std::cout << "Choices: ";
+        std::cout << std::endl << "-------------------" << std::endl;
     }
+    */
 
     return ret;
 }
@@ -85,43 +93,46 @@ bool DialogManager::LoadDialogs(string path, map<int, Dialog>& dialogs)
 {
     bool ret = true;
 
-    // TODO load dialog csv
-    rapidcsv::Document doc;
-
     try {
-        rapidcsv::Document doc(path, rapidcsv::LabelParams(0, 0));
+        rapidcsv::Document doc(path, rapidcsv::LabelParams(0, -1));
+
+        vector<int> ids = doc.GetColumn<int>("id");
+        vector<string> characters = doc.GetColumn<string>("character");
+        vector<string> ESs = doc.GetColumn<string>("ES");
+        vector<string> ENs = doc.GetColumn<string>("EN");
+        vector<string> CATs = doc.GetColumn<string>("CAT");
+        vector<string> types = doc.GetColumn<string>("type");
+        vector<string> choices = doc.GetColumn<string>("choices");
+
+        for (size_t i = 0; i < characters.size(); i++) {
+            Dialog dialog;
+            dialog.id = ids[i];
+            dialog.character = characters[i];
+            dialog.ES = ESs[i];
+            dialog.EN = ENs[i];
+            dialog.CAT = CATs[i];
+
+            // Check the type and parse it to the DialogType enum
+            if (types[i] == "D") {
+                dialog.type = DialogType::DIALOG;
+            } else if (types[i] == "C") {
+                dialog.type = DialogType::CHOICE;
+            } else {
+                std::cerr << "Invalid dialog type: " << types[i] << std::endl;
+                continue;
+            }
+
+            stringstream choicesStream(choices[i]);
+            string choice;
+            while (getline(choicesStream, choice, ',')) {
+                dialog.choices.Add(stoi(choice));
+            }
+
+            dialogs[dialog.id] = dialog;
+        }
     } catch (const std::ios_base::failure& e) {
         std::cerr << "Failed to open or read file at " << path << ": " << e.what() << std::endl;
-        // handle error or exit
         ret = false;
-    }
-
-    vector<int> ids = doc.GetColumn<int>("id");
-    vector<string> characters = doc.GetColumn<string>("character");
-    vector<string> ESs = doc.GetColumn<string>("ES");
-    vector<string> ENs = doc.GetColumn<string>("EN");
-    vector<string> CATs = doc.GetColumn<string>("CAT");
-    vector<string> types = doc.GetColumn<string>("type");
-    vector<string> choices = doc.GetColumn<string>("choices");
-
-    for (size_t i = 0; i < ids.size(); i++) {
-        Dialog dialog;
-        dialog.id = ids[i];
-        dialog.character = characters[i];
-        dialog.ES = ESs[i];
-        dialog.EN = ENs[i];
-        dialog.CAT = CATs[i];
-        //dialog.type = StringToDialogType(types[i]);
-
-        /*
-        stringstream choicesStream(choices[i]);
-        string choice;
-        while (getline(choicesStream, choice, ',')) {
-            dialog.choices.push_back(stoi(choice));
-        }
-        */
-
-        dialogs[dialog.id] = dialog;
     }
 
     return ret;
