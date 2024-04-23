e0e5c231da319b225ce3b1b0e867b5300082864b
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2024-04-23 16:05:21 +0200

    fixed gamepad movement

--- include/Gameplay/States/Player/PlayerIdleState.hpp
+++ include/Gameplay/States/Player/PlayerIdleState.hpp
@@ -1,6 +1,7 @@
 #ifndef __PLAYERIDLESTATE_H__
 #define __PLAYERIDLESTATE_H__
 
+#include "Core/App.h"
 #include "Gameplay/Entities/Player.h"
 #include "Utils/State.h"
 #include "Utils/SString.h"
@@ -18,10 +19,15 @@ public:
     }
     inline void Update(float dt) override
     {
+
+        GamePad& pad = app->input->pads[0];
+
         if (app->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT or
             app->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT or
             app->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT or
-            app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+            app->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT or
+            pad.l_x != 0.0f or
+            pad.l_y != 0.0f) {
 
             StateMachineReference->ChangeState("move");
         }
