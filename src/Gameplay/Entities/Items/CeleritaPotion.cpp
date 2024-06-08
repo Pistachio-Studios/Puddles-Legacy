c47d0976f283fc6b150ec53a303a95c63825f576
MariaPerarnau <mariaperarnausanguesa@gmail.com>
2024-06-08 21:05:00 +0200

    2 potion functionality (TODO: Call function Use() to try them)

--- src/Gameplay/Entities/Items/CeleritaPotion.cpp
+++ src/Gameplay/Entities/Items/CeleritaPotion.cpp
@@ -1,16 +1,29 @@
 #include "Gameplay/Entities/Items/CeleritaPotion.h"
 #include "Utils/Log.h"
+#include "Gameplay/Entities/Player.h"
 
 // Constructor
 CeleritaPotion::CeleritaPotion(std::string name, int quantity, std::string description) 
     : Potion(name, quantity, description) {}
 
+bool CeleritaPotion::Start() {
+    timer = Timer();
+
+    return true;
+}
 // Destructor
 CeleritaPotion::~CeleritaPotion() {}
 
 // Use the CeleritaPotion
 void CeleritaPotion::Use() {
     // Implement the logic for using the CeleritaPotion
+    Player* player; 
     LOG("Using ", name, ". ", description);
     quantity--;
+    player->maxSpeed *= 2;
+    timer.Start();
+
+    if (timer.ReadSec() >= potionDuration) {
+        player->maxSpeed /= 2; // Restore original speed after duration
+    }
 }
--- src/Gameplay/Entities/Items/VitaPotion.cpp
+++ src/Gameplay/Entities/Items/VitaPotion.cpp
@@ -1,4 +1,5 @@
 #include "Gameplay/Entities/Items/VitaPotion.h"
+#include "Gameplay/Entities/Player.h"
 #include "Utils/Log.h"
 
 // Constructor
@@ -8,9 +9,24 @@ VitaPotion::VitaPotion(std::string name, int quantity, std::string description)
 // Destructor
 VitaPotion::~VitaPotion() {}
 
+bool VitaPotion::Start() {
+    timer = Timer();
+
+    return true;
+}
+
+class Player;
 // Use the vita potion
 void VitaPotion::Use() {
     // Implement the logic for using the vita potion
+    Player* player;
     LOG("Using ", name, ". ", description);
     quantity--;
+    player->vida = player->totalLivesPlayer;
+	player->maxSpeed *= 2;
+	timer.Start();
+
+    if (timer.ReadSec() >= potionDuration) {
+        player->maxSpeed /= 2; // Restore original speed after duration
+    }
 }
