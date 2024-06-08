b8b058c8339f4d5d0908adbe6ec2868977c0733e
MariaPerarnau <mariaperarnausanguesa@gmail.com>
2024-06-08 21:05:38 +0200

    ?

--- src/Gameplay/Entities/Items/VitaPotion.cpp
+++ src/Gameplay/Entities/Items/VitaPotion.cpp
@@ -9,13 +9,6 @@ VitaPotion::VitaPotion(std::string name, int quantity, std::string description)
 // Destructor
 VitaPotion::~VitaPotion() {}
 
-bool VitaPotion::Start() {
-    timer = Timer();
-
-    return true;
-}
-
-class Player;
 // Use the vita potion
 void VitaPotion::Use() {
     // Implement the logic for using the vita potion
@@ -23,10 +16,4 @@ void VitaPotion::Use() {
     LOG("Using ", name, ". ", description);
     quantity--;
     player->vida = player->totalLivesPlayer;
-	player->maxSpeed *= 2;
-	timer.Start();
-
-    if (timer.ReadSec() >= potionDuration) {
-        player->maxSpeed /= 2; // Restore original speed after duration
-    }
 }
